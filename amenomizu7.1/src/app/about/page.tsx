8905e419773d37263d7af376a59e9b0442cdb4b0
Nakayama <123441983+Shohei-Nakayama@users.noreply.github.com>
2025-09-14 16:32:09 +0900

    add button to back in about page

--- amenomizu7.1/src/app/about/page.tsx
+++ amenomizu7.1/src/app/about/page.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link'
+
+
+
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>これはAboutページです</p>
+    <div className = "font-sans min-h-screen p-8">
+      <main className = "max-w-4xl mx-auto">
+          <h1 className = "text-4xl font-bold mb-4">About Page</h1>
+          <p className = "text-lg mb-8">これはAboutページです</p>
+
+        <Link href = "/">
+          <button className = "bg-gray-500.hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" > 
+            ← ホームに戻る
+          </button>
+        </Link>
+      </main>
     </div>
   )
 }
