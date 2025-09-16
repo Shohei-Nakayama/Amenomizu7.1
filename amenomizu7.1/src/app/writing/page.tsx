64afd2783a70160f2c04b3d21d357b4987849549
Nakayama <123441983+Shohei-Nakayama@users.noreply.github.com>
2025-09-16 15:51:16 +0900

    added profile page and writing page

---
+++ amenomizu7.1/src/app/writing/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function writing() {
+  return (
+    <div className="font-sans min-h-screen p-8">
+      <main className="max-w-4xl mx-auto">
+        <h1 className="text-4xl font-bold mb-4">writing Page</h1>
+        <p className="text-lg mb-8">これはwritingページです</p>
+
+        <Link href="/">
+          <button className="bg-gray-400 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+            ← ホームに戻る
+          </button>
+        </Link>
+      </main>
+    </div>
+  );
+}
