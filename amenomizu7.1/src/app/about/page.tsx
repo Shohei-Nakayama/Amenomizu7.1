24e1e9028c2d0d8b634cb0e0568f8721b4efce93
Nakayama <123441983+Shohei-Nakayama@users.noreply.github.com>
2025-09-23 14:15:24 +0900

    added concept page's sentence

--- amenomizu7.1/src/app/about/page.tsx
+++ amenomizu7.1/src/app/about/page.tsx
@@ -21,9 +21,31 @@ export default function About() {
             width: "100%",
           }}
         ></div>
-
-        <h1 className="text-4xl font-bold mb-4">About Page</h1>
-        <p className="text-lg mb-8">これはAboutページです</p>
+        <br />
+        <h1 className="text-4xl font-bold mb-4">あめのみづ鍼灸院について</h1>
+        <br />
+        <p className="text-lg mb-8">
+          あめのみづ鍼灸院は 看板もありません。 <br />
+          特に広告も出していません。 <br />
+          ただ、いらっしゃる方のお話をお伺いし
+          <br />
+          そこからお身体としての訴えを聞くことを 大事にしています。
+          <br />
+          <br />
+          できる限り、対処療法ではなく その原因となるものがどこにあるのか。
+          <br />
+          日々でどんなことに気をつけたらいいのかなどを
+          <br />
+          包括的な視点から見て
+          <br />
+          ひとりひとりに最適な形の治療をしていきたい。
+          <br />
+          とにかく質の良い治療をひとりひとりに行っていきたい。
+          <br />
+          そう思って日々施術をしています。 <br />
+          <br />
+          あめのみづ鍼灸院 中山　翔平
+        </p>
 
         <Link href="/">
           <button className="bg-gray-400 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
