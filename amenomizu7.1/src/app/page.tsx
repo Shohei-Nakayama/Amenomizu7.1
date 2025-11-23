4b8c4385c1dcedf9b04a687a236f6e9870e22c69
なかやま <Nakayama@nakayama-no-MacBook-Air.local>
2025-11-23 17:44:11 +0900

    cleaned top page

--- amenomizu7.1/src/app/page.tsx
+++ amenomizu7.1/src/app/page.tsx
@@ -151,14 +151,6 @@ export default function Home() {
                         私について
                       </Link>
                     </li>
-                    <li className="mb-2 tracking-[-.01em]">
-                      <Link
-                        href="/writing"
-                        className="hover:text-yellow-500 transition-colors"
-                      >
-                        医術ノート（最新）
-                      </Link>
-                    </li>
                   </ol>
                 </nav>
 
@@ -195,7 +187,7 @@ export default function Home() {
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    Blog
+                    日々のこと
                   </a>
                 </div>
               </main>
@@ -236,56 +228,6 @@ export default function Home() {
             </div>
           </div>
 
-          {/* メインコンテンツ部分（フッター） */}
-          <div className="px-8 pb-20 pt-8 sm:px-20">
-            <footer className="flex gap-[24px] flex-wrap items-center justify-center">
-              <a
-                className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  aria-hidden
-                  src="/file.svg"
-                  alt="File icon"
-                  width={16}
-                  height={16}
-                />
-                Learn
-              </a>
-              <a
-                className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  aria-hidden
-                  src="/window.svg"
-                  alt="Window icon"
-                  width={16}
-                  height={16}
-                />
-                Examples
-              </a>
-              <a
-                className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Image
-                  aria-hidden
-                  src="/globe.svg"
-                  alt="Globe icon"
-                  width={16}
-                  height={16}
-                />
-                Go to nextjs.org →
-              </a>
-            </footer>
-          </div>
         </div>
       )}
     </>
