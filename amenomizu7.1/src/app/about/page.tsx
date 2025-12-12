4b64d9c9d12daf823e75d6b4e40f42be88086cc8
なかやま <Nakayama@nakayama-no-MacBook-Air.local>
2025-12-13 00:10:07 +0900

    modefied some

--- amenomizu7.1/src/app/about/page.tsx
+++ amenomizu7.1/src/app/about/page.tsx
@@ -7,7 +7,9 @@ export default function About() {
 
       <main className="max-w-4xl mx-auto px-8 py-12 text-center">
         <section className="mb-12">
-          <h2 className="text-2xl font-semibold mb-6">あめのみづ鍼灸院について</h2>
+          <h2 className="text-2xl font-semibold mb-6">
+            あめのみづ鍼灸院について
+          </h2>
           <p className="text-lg mb-4">
             あめのみづ鍼灸院は 看板もありません。 特に広告も出していません。
           </p>
@@ -20,7 +22,7 @@ export default function About() {
         <section className="mb-12">
           <h2 className="text-2xl font-semibold mb-6">治療へのアプローチ</h2>
           <p className="text-lg mb-4">
-            できる限り、対処療法ではなく その原因となるものがどこにあるのか。
+            できる限り、対症療法ではなく その原因となるものがどこにあるのか。
           </p>
           <p className="text-lg mb-4">
             日々でどんなことに気をつけたらいいのかなどを包括的な視点から見て、ひとりひとりに最適な形の治療をしていきたい。
